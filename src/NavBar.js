3dffefeba0df7c9aea024ce6de99b1fe82e1ab4c
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-06-23 11:32:28 -0400

    testing commit

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,7 @@ import React from "react";
 function NavBar() {
     return (
         <div class="navbar">
+            <h1>NavBar</h1>
             <a href="/.movies">Movies</a>
             <a href="/.form">Add New Movie</a>
             <a href="/.search">Search Movies</a>
