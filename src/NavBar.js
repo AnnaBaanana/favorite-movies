26122c77f45e4681735e6ac01244b2d39fdfb7a2
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-06-23 11:22:22 -0400

    created a navbar component

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <div class="navbar">
+            <a href="/.movies">Movies</a>
+            <a href="/.form">Add New Movie</a>
+            <a href="/.search">Search Movies</a>
+        </div>
+    )
+}
+
+export default NavBar;
