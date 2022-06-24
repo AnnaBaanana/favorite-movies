85d4dad1b981356219f51ea9721ef7742ea3784e
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-06-23 20:26:32 -0400

    created basic componets for MovieBar, MovieList and MoviePage

---
+++ src/MovieBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function MovieBar() {
+
+    return <div>MovieBar</div>
+
+}
+
+export default MovieBar;
+
---
+++ src/MovieList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MovieList() {
+    return null
+}
+
+export default MovieList;
